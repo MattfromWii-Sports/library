470b4e9593d72903f7744a380c7aa193396127e0
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-07-24 20:44:37 +0800

    Create Variables For Form Values, Outline Necessary Functions, And Set Reset All Function

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let myLibrary = [];
 
-const addNewBook = document.getElementById('add-btn');
+const addNewBookBtn = document.getElementById('add-btn');
 const dialog = document.getElementById('dialog-book');
+const form = document.querySelector('form');
 
-addNewBook.addEventListener('mousedown', function() {
+const titleForm = document.querySelector('#title');
+const authorForm = document.querySelector('#author');
+const pagesForm = document.querySelector('#pages');
+const readForm = document.querySelector('#read');
+
+addNewBookBtn.addEventListener('mousedown', function() {
     dialog.showModal();
 })
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    createPages();
+    resetAll();
+    dialog.close();
+});
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,5 +28,16 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
+    myLibrary.push(new Book(titleForm.value, authorForm.value, pagesForm.value, readForm.checked));
+}
 
+function createPages() {
+
+}
+
+function resetAll() {
+    titleForm.value = '';
+    authorForm.value = '';
+    pagesForm.value = '';
+    readForm.checked = false;
 }

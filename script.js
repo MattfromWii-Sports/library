9cf79295d423cd7ed8c5c43f291f90ef83aeb89a
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-07-25 21:43:22 +0800

    Add Js For Constructing Cards,And Fix Card Structure And Styles

--- script.js
+++ script.js
@@ -31,8 +31,39 @@ function addBookToLibrary() {
     myLibrary.push(new Book(titleForm.value, authorForm.value, pagesForm.value, readForm.checked));
 }
 
-function createPages() {
+const pageContainer = document.querySelector('.card-container');
 
+function createPages() {
+    //Need to add remove all cards function
+    for (let i=0; i<myLibrary.length; i++) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.index = `${i}`;
+        pageContainer.appendChild(card);
+        let cardTitle = document.createElement('h2');
+        let cardAuthor = document.createElement('h3');
+        let cardPages = document.createElement('p');
+        cardTitle.textContent = myLibrary[i].title;
+        cardAuthor.textContent = myLibrary[i].author;
+        cardPages.textContent = `${myLibrary[i].pages} pages`;
+        card.appendChild(cardTitle);
+        card.appendChild(cardAuthor);
+        card.appendChild(cardPages);
+        let cardButtons = document.createElement('div');
+        card.appendChild(cardButtons);
+        let cardRead = document.createElement('button');
+        let cardRemove = document.createElement('button');
+        if (myLibrary[i].read === true) {
+            cardRead.textContent = "Read";
+            cardRead.classList.add('read-btn');
+        } else {
+            cardRead.textContent = "Not Read";
+            cardRead.classList.add('notread-btn');
+        }
+        cardRemove.textContent = "Remove";
+        cardButtons.appendChild(cardRead);
+        cardButtons.appendChild(cardRemove);
+    }
 }
 
 function resetAll() {
